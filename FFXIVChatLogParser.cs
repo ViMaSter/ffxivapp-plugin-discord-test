ced22f5b8bbebf3ee17826c1df0ae787f566681e
Vincent Mahnke <vincent@mahn.ke>
2020-07-09 22:39:38 +0200

    Added test case for differently formatted items Addressed minor code style consistencies

--- FFXIVChatLogParser.cs
+++ FFXIVChatLogParser.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using FFXIVAPP.IPluginInterface;
-using FFXIVAPP.IPluginInterface.Events;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Test.Bot.FFXIV
+﻿namespace FFXIVAPP.Plugin.Discord.Test
 {
-    class FakePluginHost : FFXIVAPP.IPluginInterface.IPluginHost
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FFXIVAPP.IPluginInterface;
+    using FFXIVAPP.IPluginInterface.Events;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    class FakePluginHost : IPluginHost
     {
         event EventHandler<ActionContainersEvent> IPluginHost.ActionContainersUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         public event EventHandler<ChatLogItemEvent> ChatLogItemReceived;
@@ -29,207 +25,106 @@ namespace Test.Bot.FFXIV
 
         event EventHandler<InventoryContainersEvent> IPluginHost.InventoryContainersUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsAddedEvent> IPluginHost.MonsterItemsAdded
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsRemovedEvent> IPluginHost.MonsterItemsRemoved
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsEvent> IPluginHost.MonsterItemsUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<NetworkPacketEvent> IPluginHost.NetworkPacketReceived
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsAddedEvent> IPluginHost.NPCItemsAdded
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsRemovedEvent> IPluginHost.NPCItemsRemoved
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsEvent> IPluginHost.NPCItemsUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<PartyMembersAddedEvent> IPluginHost.PartyMembersAdded
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<PartyMembersRemovedEvent> IPluginHost.PartyMembersRemoved
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<PartyMembersEvent> IPluginHost.PartyMembersUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsAddedEvent> IPluginHost.PCItemsAdded
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsRemovedEvent> IPluginHost.PCItemsRemoved
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<ActorItemsEvent> IPluginHost.PCItemsUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
         event EventHandler<TargetInfoEvent> IPluginHost.TargetInfoUpdated
         {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
+            add => throw new NotImplementedException();
+            remove => throw new NotImplementedException();
         }
 
-        void IPluginHost.PopupMessage(string pluginName, FFXIVAPP.Common.Models.PopupContent content)
+        void IPluginHost.PopupMessage(string pluginName, Common.Models.PopupContent content)
         {
             throw new NotImplementedException();
         }
 
         public void SendChatlogMessage(byte[] message)
         {
-            ChatLogItemReceived.Invoke(this, new ChatLogItemEvent(this, new Sharlayan.Core.ChatLogItem()
+            if (this.ChatLogItemReceived == null)
+            {
+                throw new NotImplementedException("Handler for receiving chat log items needs to be set up in order to properly execute tests");
+            }
+            this.ChatLogItemReceived.Invoke(this, new ChatLogItemEvent(this, new Sharlayan.Core.ChatLogItem()
             {
                 Bytes = message,
                 Code = "",
@@ -239,62 +134,77 @@ namespace Test.Bot.FFXIV
 
         public void UpdateWorldName(string worldName)
         {
-            ConstantsUpdated.Invoke(this, new ConstantsEntityEvent(this, new FFXIVAPP.Common.Core.Constant.ConstantsEntity
+            if (this.ConstantsUpdated == null)
+            {
+                throw new NotImplementedException("Handler for constants updates needs to be set up in order to properly execute tests");
+            }
+            this.ConstantsUpdated.Invoke(this, new ConstantsEntityEvent(this, new Common.Core.Constant.ConstantsEntity
             {
                 ServerName = worldName
             }));
         }
 
-        internal void InitConsts()
+        internal void InitConstants()
         {
-            UpdateWorldName("Cerberus");
-            CurrentPlayerUpdated.Invoke(this, new CurrentPlayerEvent(this, new Sharlayan.Core.CurrentPlayer
+            this.UpdateWorldName("Cerberus");
+
+            if (this.CurrentPlayerUpdated == null)
+            {
+                throw new NotImplementedException("Handler for player updates needs to be set up in order to properly execute tests");
+            }
+            this.CurrentPlayerUpdated.Invoke(this, new CurrentPlayerEvent(this, new Sharlayan.Core.CurrentPlayer
             {
                 Name = "Dis Cord"
             }));
         }
     }
 
-    public class FakeDiscordHandler : FFXIVAPP.Plugin.Discord.ChatHandler.IDiscord
+    public class FakeDiscordHandler : ChatHandler.IDiscord
     {
-        public void Broadcast(string message)
+#pragma warning disable 1998
+        public async Task Broadcast(string message)
         {
-            brodcastMessages.Push(message);
+            this._broadcastMessages.Push(message);
         }
+#pragma warning restore 1998
 
         public void SetIsActive(bool active)
         {
             throw new NotImplementedException();
         }
 
-        private Stack<string> brodcastMessages = new Stack<string>(1);
+        private readonly Stack<string> _broadcastMessages = new Stack<string>(1);
         public byte[] RetrieveLastBroadcastAsUTF8()
         {
-            return System.Text.Encoding.UTF8.GetBytes(brodcastMessages.Pop());
+            return System.Text.Encoding.UTF8.GetBytes(this._broadcastMessages.Pop());
         }
     }
 
     [TestClass]
     public class ChatLogParserTest
     {
-        private static FakePluginHost fakePluginHost;
-        private static FakeDiscordHandler fakeDiscordHandler;
-        private static FFXIVAPP.Plugin.Discord.ChatHandler.FFXIV FFXIVHandler;
+        private static FakePluginHost _fakePluginHost;
+        private static FakeDiscordHandler _fakeDiscordHandler;
+        private static ChatHandler.FFXIV _ffxivHandler;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            fakePluginHost = new FakePluginHost();
-            fakeDiscordHandler = new FakeDiscordHandler();
-            FFXIVHandler = new FFXIVAPP.Plugin.Discord.ChatHandler.FFXIV(fakePluginHost);
-            fakePluginHost.InitConsts();
-            FFXIVHandler.MainAsync(fakeDiscordHandler).Wait();
+            if (context == null) {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            _fakePluginHost = new FakePluginHost();
+            _fakeDiscordHandler = new FakeDiscordHandler();
+            _ffxivHandler = new ChatHandler.FFXIV(_fakePluginHost);
+            _fakePluginHost.InitConstants();
+            _ffxivHandler.MainAsync(_fakeDiscordHandler).Wait();
         }
 
         [TestInitialize]
         public void ResetTestEnvironment()
         {
-            fakePluginHost.UpdateWorldName("Cerberus");
+            _fakePluginHost.UpdateWorldName("Cerberus");
         }
 
         private static byte[] ExtractResource(string filename)
@@ -314,14 +224,14 @@ namespace Test.Bot.FFXIV
         }
 
         /// <summary>
-        /// A specfic set of byte streams generated by FFXIV will be converted to a human readable representation
+        /// A specific set of byte streams generated by FFXIV will be converted to a human readable representation
         /// </summary>
         [TestMethod]
         [DynamicData(nameof(GetChatLogEntries), DynamicDataSourceType.Method)]
         public void UserReadableOutput(string fileName, byte[] rawData, byte[] resultData)
         {
-            fakePluginHost.SendChatlogMessage(rawData);
-            CollectionAssert.AreEqual(resultData, fakeDiscordHandler.RetrieveLastBroadcastAsUTF8());
+            _fakePluginHost.SendChatlogMessage(rawData);
+            CollectionAssert.AreEqual(resultData, _fakeDiscordHandler.RetrieveLastBroadcastAsUTF8());
         }
 
         public static IEnumerable<object[]> GetChatLogEntries()
@@ -342,7 +252,7 @@ namespace Test.Bot.FFXIV
         [TestMethod]
         public void AppendBotWorldName()
         {
-            string newWorldName = "clearlydifferent";
+            string newWorldName = "clearly_different";
 
             var rawData = ExtractResource("FFXIVAPP.Plugin.Discord.Test.TestData.FFXIVChat.localuser.binary");
             var resultData = System.Text.Encoding.UTF8.GetBytes(
@@ -351,11 +261,11 @@ namespace Test.Bot.FFXIV
                 ).Replace("@Cerberus", "@"+ newWorldName)
             );
 
-            fakePluginHost.UpdateWorldName(newWorldName);
+            _fakePluginHost.UpdateWorldName(newWorldName);
 
-            fakePluginHost.SendChatlogMessage(rawData);
+            _fakePluginHost.SendChatlogMessage(rawData);
 
-            CollectionAssert.AreEqual(resultData, fakeDiscordHandler.RetrieveLastBroadcastAsUTF8());
+            CollectionAssert.AreEqual(resultData, _fakeDiscordHandler.RetrieveLastBroadcastAsUTF8());
         }
     }
 }
